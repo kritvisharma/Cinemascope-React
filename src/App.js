101f1d289e7b5df7296baa696b4a967080fdf20a
Kritvi Sharma <kritvisharma0912@gmail.com>
2024-03-25 00:19:39 +0400

    Excess files deletion 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {useEffect} from 'react';
+
+const API_URL= 'http://www.omdbapi.com?apikey=3bd870a9';
+
+
+
+const App =()=>{
+    const searchMovies = async(title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    useEffect(()=>{
+        searchMovies('Harry Potter');
+    },[])
+
+    return(
+        <h1>App</h1>
+    );
 }
 
 export default App;
